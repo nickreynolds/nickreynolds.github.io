952f8f4d988d9950f01ad9da821b900894687b09
nickreynolds <nicholas.s.reynolds@gmail.com>
2024-03-26 19:00:54 -0400

    feat: make changes to fit me

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -3,19 +3,29 @@ import IconLink from "./IconLink";
 import {Box} from "@mui/material";
 
 function PortfolioBlock(props) {
-   const {image, live, source, title} = props;
+   const {image, live, blogpost, source, title} = props;
+   console.log("live: ", live)
    return (
       <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
          <Box component={'img'} src={image} alt={'mockup'}/>
          <h1 style={{fontSize: '2rem'}}>{title}</h1>
          <Box className={'portfolio'} display={'flex'} flexDirection={'column'} gap={'0.5rem'}
               alignItems={'center'} fontSize={'1.5rem'} py={'2rem'}>
+            {live && (
             <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
                <IconLink link={live} title={'Live Demo'} icon={'fa fa-safari'}/>
             </Box>
+            )}
+            {blogpost && (
+            <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
+               <IconLink link={blogpost} title={'Blog Post'} icon={'fa fa-medium'}/>
+            </Box>
+            )}
+            {source && (
             <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
                <IconLink link={source} title={'Source Code'} icon={'fa fa-code'}/>
             </Box>
+            )}
          </Box>
       </Box>
    );
--- src/info/Info.js
+++ src/info/Info.js
@@ -1,9 +1,6 @@
-import self from "../img/self.png"
-import mock1 from "../img/mock1.png"
-import mock2 from "../img/mock2.png"
-import mock3 from "../img/mock3.png"
-import mock4 from "../img/mock4.png"
-import mock5 from "../img/mock5.png"
+import self from "../img/self4.png"
+import mock1 from "../img/veramo1.png"
+import mock2 from "../img/veramo2.png"
 
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
@@ -23,17 +20,17 @@ of what each of the values mean.
  */
 
 export const info = {
-    firstName: "John",
-    lastName: "Smith",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
-    position: "a Full Stack Developer",
+    firstName: "Nick",
+    lastName: "Reynolds",
+    initials: "NR", // the example uses first and last, but feel free to use three or more if you like.
+    position: "a Full Stack Developer and Product Manager",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
     baseColor: colors[0],
     miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
         {
-            emoji: '☕',
-            text: 'fueled by coffee'
+            emoji: '🕵️',
+            text: 'building Agency'
         },
         {
             emoji: '🌎',
@@ -41,58 +38,43 @@ export const info = {
         },
         {
             emoji: "💼",
-            text: "Systems Engineer at Google"
+            text: "Identity Lead at Consensys"
         },
         {
             emoji: "📧",
-            text: "johnsmith@gmail.com"
+            text: "nicholas.s.reynolds@gmail.com"
         }
     ],
     socials: [
         {
-            link: "https://facebook.com",
-            icon: 'fa fa-facebook',
-            label: 'facebook'
-        },
-        {
-            link: "https://instagram.com",
-            icon: 'fa fa-instagram',
-            label: 'instagram'
-        },
-        {
-            link: "https://github.com",
+            link: "https://github.com/nickreynolds",
             icon: "fa fa-github",
             label: 'github'
         },
         {
-            link: "https://linkedin.com",
+            link: "https://www.linkedin.com/in/reynoldsnick/",
             icon: "fa fa-linkedin",
             label: 'linkedin'
-        },
-        {
-            link: "https://twitter.com",
-            icon: "fa fa-twitter",
-            label: 'twitter'
         }
 // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
 // Just change the links so that they lead to your social profiles.
 
     ],
-    bio: "Hello! I'm John. I'm a systems engineer for Google. I studied CompSci at Harvard, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+    bio: "Hi! I'm Nick. I lead Identity R&D at Consensys Mesh, studied Digital Media Design at the University of Pennsylvania, and work to build a better internet, with more agency, using verifiable data.",
     skills:
         {
-            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma'],
-            exposedTo: ['nodejs', 'python', 'adobe illustrator']
+            proficientWith: ['javascript', 'nodejs', 'react (browser & mobile)', 'typescript', 'solidity', 'golang'],
+            exposedTo: ['rust', 'python']
         }
     ,
     hobbies: [
         {
-            label: 'reading',
-            emoji: '📖'
+            label: 'climbing',
+            emoji: '🧗🏼'
         },
         {
-            label: 'theater',
-            emoji: '🎭'
+            label: 'biking',
+            emoji: '🚴🏼'
         },
         {
             label: 'movies',
@@ -107,34 +89,14 @@ export const info = {
     ],
     portfolio: [ // This is where your portfolio projects will be detailed
         {
-            title: "Project 1",
-            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
-            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
+            title: "Veramo",
+            source: "https://github.com/decentralized-identity/veramo", // this should be a link to the **repository** of the project, where the code is hosted.
             image: mock1
         },
         {
-            title: "Project 2",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
+            title: "Bringing Identity to AI",
+            blogpost: "https://medium.com/veramo/veramo-didcomm-bringing-identity-to-ai-f7efd1fbdf5d",
             image: mock2
-        },
-        {
-            title: "Project 3",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock3
-        },
-        {
-            title: "Project 4",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock4
-        },
-        {
-            title: "Project 5",
-            live: "https://paytonpierce.dev",
-            source: "https://github.com/paytonjewell",
-            image: mock5
         }
     ]
 }
